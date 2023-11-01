507f9240bc8de7c451241b77df1173ef2b13b73c
MADHUVEER <111329658+KoilakondaMadhu@users.noreply.github.com>
2023-11-01 14:48:34 +0530

    Update sumofeveninlist.py

--- sumofeveninlist.py
+++ sumofeveninlist.py
@@ -3,3 +3,16 @@
 # in the list
 # ip = 1 2 3 4 5 6 
 # op = 12(2+4+6)
+from typing import List
+
+def sum_even(a: List[int]) -> int:
+    b = 0  # Initialize a variable to store the sum of even numbers
+    for i in a:
+        if i % 2 == 0:  # Check if the number is even
+            b += i  # Add the even number to the sum
+    return b  # Return the sum of even numbers
+
+# Example usage:
+input_list = [1, 2, 3, 4, 5, 6]
+result = sum_even(input_list)
+print(result)  # This will print 12, which is the sum of even numbers (2 + 4 + 6) in the list.
