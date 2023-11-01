42c2fcfdcf5b370a9d05b01aaef1a322a90ce146
MADHUVEER <111329658+KoilakondaMadhu@users.noreply.github.com>
2023-11-01 15:40:57 +0530

    Create password validator.py

---
+++ password validator.py
@@ -0,0 +1,29 @@
+# Question 9: Password Validator
+
+# You are given a string str. Your task is to find the number of checks that are not satisfied by the string str by checking the validation of the string str if it's a strong password or not. A
+
+# password is a strong password only if:
+
+# ⚫ It contains at least 6 characters and at most 22 characters.
+
+# It must contain at least one uppercase character.
+
+# ⚫ It must contain at least one lowercase character.
+
+# It must contain at least one special character (@, !, &, ^, %, *. #).
+
+# It must contain at least one numeric value.
+
+# It must not contain any two same consecutive characters.
+
+# Input Format:
+
+# ⚫ The input consists of a single line:
+
+# The first line contains a string str.
+
+# Output Format:
+
+# 0 Print the number that represents the number of checks it is not satisfied by the str.
+
+# Constraints
