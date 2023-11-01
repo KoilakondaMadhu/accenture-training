8d0c893cd39833eae6a396fe49d93de66ce6bb7e
MADHUVEER <111329658+KoilakondaMadhu@users.noreply.github.com>
2023-11-01 15:32:00 +0530

    Create a.py

---
+++ a.py
@@ -0,0 +1,32 @@
+# Alice wins N candies after securing the first rank in the coding contest. Now to enjoy the moment he wants to share some candies with his friend, Bob. He wants to have a greater number of candies than Bob but additionally, he wants to give at least 1 candy to Bob. He asked you to find the number of ways he can share the candies with Bob. Formally find the number of ways to share candies such that N=A+B, (A> B).
+
+# Input Format:
+
+# • The input consists of a single line. The first line contains an integer N.
+
+# Output Format:
+
+# Print the number that represents the number of ways to share the candies.
+
+# Constraints:
+
+# 1s NS 10^9
+
+# Example:
+
+# Input:
+
+# 7
+
+# Output:
+
+# 3
+
+# Explanation:
+
+n = int(input())
+if n%2 ==0:
+  print(n//2)
+else:
+  print(n//2)
+  
