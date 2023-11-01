fd3b19b010f203d78a369b931bcf4b5059273f2f
MADHUVEER <111329658+KoilakondaMadhu@users.noreply.github.com>
2023-11-01 15:49:32 +0530

    Update password validator.py

--- password validator.py
+++ password validator.py
@@ -27,3 +27,92 @@
 # 0 Print the number that represents the number of checks it is not satisfied by the str.
 
 # Constraints
+
+
+
+s = input()
+check = 0
+
+if not (6 <= len(s) <= 22):
+    check += 1
+
+up = 0
+lo = 0
+d = 0
+sp = 0
+
+for i in range(len(s)):
+    if s[i].isupper():
+        up += 1
+    elif s[i].islower():
+        lo += 1
+    elif s[i].isdigit():
+        d += 1
+    else:
+        sp += 1
+
+if up < 1:
+    check += 1
+if lo < 1:
+    check += 1
+if d < 1:
+    check += 1
+if sp < 1:
+    check += 1
+
+for i in range(len(s) - 1):
+    if s[i] == s[i + 1]:
+        check += 1
+        break
+
+print(check)
+
+
+
+
+
+
+
+
+
+
+
+
+# def is_valid_password(s):
+#     checks_not_satisfied = 0
+
+#     # Check if the length is within the required range.
+#     if not (6 <= len(s) <= 22):
+#         checks_not_satisfied += 1
+
+#     # Check for at least one uppercase character.
+#     if not any(char.isupper() for char in s):
+#         checks_not_satisfied += 1
+
+#     # Check for at least one lowercase character.
+#     if not any(char.islower() for char in s):
+#         checks_not_satisfied += 1
+
+#     # Check for at least one special character.
+#     special_characters = set('@!&^%*#')
+#     if not any(char in special_characters for char in s):
+#         checks_not_satisfied += 1
+
+#     # Check for at least one numeric value.
+#     if not any(char.isdigit() for char in s):
+#         checks_not_satisfied += 1
+
+#     # Check for consecutive characters.
+#     for i in range(1, len(s)):
+#         if s[i] == s[i - 1]:
+#             checks_not_satisfied += 1
+#             break  # We only need to count this once.
+
+#     return checks_not_satisfied
+
+# # Input
+# password = input()
+
+# # Check and print the number of checks not satisfied.
+# checks = is_valid_password(password)
+# print(checks)
