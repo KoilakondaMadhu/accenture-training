a9e7e9fb9451e3fd0cc14db74d29907a962f963d
MADHUVEER <111329658+KoilakondaMadhu@users.noreply.github.com>
2023-11-01 14:44:23 +0530

    Create sumofeveninlist.py

---
+++ sumofeveninlist.py
@@ -0,0 +1,5 @@
+# write the fuction to calculate the sum of the even numbers in the given list of integers.
+# the funtion shounld take a list of integers as input and return the sum of all even numbers
+# in the list
+# ip = 1 2 3 4 5 6 
+# op = 12(2+4+6)
