3db09b7ba3efd931fefc288c811b22e1985071a3
MADHUVEER <111329658+KoilakondaMadhu@users.noreply.github.com>
2023-11-09 12:20:12 +0530

    Update reverse an int .py

--- reverse an int .py
+++ reverse an int .py
@@ -1,10 +1,12 @@
-n = 1010
-val = 0
-sum1 =0
-while (n>0):
-  rem = n%10
-  res = rem*pow(2,val)
-  val += 1
-  sum += res
-  n = n//10
-print(sum)
+num = int(input("Enter an integer: "))  # Input the integer
+reversed_num = 0
+
+while num > 0:
+    # Get the last digit of the number
+    last_digit = num % 10
+    # Append the last digit to the reversed number
+    reversed_num = reversed_num * 10 + last_digit
+    # Remove the last digit from the original number
+    num = num // 10
+
+print("Reversed integer:", reversed_num)
